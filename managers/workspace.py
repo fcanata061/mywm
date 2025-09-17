cbaf86e54f1632480f6c9fa8277da32d4dde32b7
fcanata061 <fcanata61@gmail.com>
2025-09-17 08:04:28 -0300

    Update workspace.py

--- managers/workspace.py
+++ managers/workspace.py
@@ -16,6 +16,7 @@ class Workspace:
     def add_window(self, win):
         self.windows.append(win)
         self.focus_index = len(self.windows) - 1
+        self.update_focus()
         self.apply_layout()
 
     def remove_window(self, win):
@@ -24,22 +25,29 @@ class Workspace:
             self.windows.remove(win)
             if self.focus_index >= len(self.windows):
                 self.focus_index = len(self.windows) - 1
+            self.update_focus()
             self.apply_layout()
 
     def focus_next(self):
         if self.windows:
             self.focus_index = (self.focus_index + 1) % len(self.windows)
+            self.update_focus()
             self.apply_layout()
 
     def focus_prev(self):
         if self.windows:
             self.focus_index = (self.focus_index - 1) % len(self.windows)
+            self.update_focus()
             self.apply_layout()
 
     def get_focused_window(self):
         if self.windows:
             return self.windows[self.focus_index]
         return None
+
+    def update_focus(self):
+        for i, w in enumerate(self.windows):
+            w.set_focus(i == self.focus_index)
 
     def apply_layout(self):
         layout = self.layouts.get(self.layout_name, TilingLayout())
