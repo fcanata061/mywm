5c1903f4371a4b14499412288928b7df7add2679
fcanata061 <fcanata61@gmail.com>
2025-09-17 08:17:41 -0300

    Update lemonbar.py

--- utils/lemonbar.py
+++ utils/lemonbar.py
@@ -2,10 +2,10 @@ import subprocess
 
 _proc = None
 
-def start(cfg):
+def start(cfg=None):
     global _proc
     if not _proc:
-        args = ["lemonbar", "-p", "-f", cfg["bar"]["font"]]
+        args = ["lemonbar", "-p", "-f", "Monospace-10"]
         _proc = subprocess.Popen(args)
 
 def stop():
