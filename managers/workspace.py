6e157a113fa373ac8e60e24b65cb90f8c8d88a42
fcanata061 <fcanata61@gmail.com>
2025-09-17 07:51:25 -0300

    Update workspace.py

--- managers/workspace.py
+++ managers/workspace.py
@@ -1,15 +1,46 @@
+from managers.window import Window
+from layouts.floating import FloatingLayout
+from layouts.tiling import TilingLayout
+
 class Workspace:
     def __init__(self, wid, layout="tiling"):
         self.id = wid
-        self.layout = layout
         self.windows = []
+        self.focus_index = 0
+        self.layout_name = layout
+        self.layouts = {
+            "floating": FloatingLayout(),
+            "tiling": TilingLayout()
+        }
 
     def add_window(self, win):
         self.windows.append(win)
+        self.focus_index = len(self.windows) - 1
+        self.apply_layout()
 
     def remove_window(self, win):
-        self.windows.remove(win)
+        if win in self.windows:
+            idx = self.windows.index(win)
+            self.windows.remove(win)
+            if self.focus_index >= len(self.windows):
+                self.focus_index = len(self.windows) - 1
+            self.apply_layout()
+
+    def focus_next(self):
+        if self.windows:
+            self.focus_index = (self.focus_index + 1) % len(self.windows)
+            self.apply_layout()
+
+    def focus_prev(self):
+        if self.windows:
+            self.focus_index = (self.focus_index - 1) % len(self.windows)
+            self.apply_layout()
+
+    def get_focused_window(self):
+        if self.windows:
+            return self.windows[self.focus_index]
+        return None
 
     def apply_layout(self):
-        # chamar layout correspondente
-        pass
+        layout = self.layouts.get(self.layout_name, TilingLayout())
+        layout.apply(self.windows)
